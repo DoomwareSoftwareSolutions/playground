77da95d9d99be929890cdbbc18e6f95bdd0a08c2
tomasboccardo <tomasboccardo@gmail.com>
2013-09-09 18:33:28 -0300

    Documentacion de las views de authentication

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -8,7 +8,9 @@ from webapp.settings.debug import *
 from apps.authentication.models import User
 from lib.utils import Crypt
 
-# Create your views here.
+# ########################################################################################### #
+# ##################################    SIGNIN VIEW     ##################################### #
+# ########################################################################################### #
 
 # Esta view maneja '/signin'. Renderea el formulario de inicio de sesion y valida los datos ingresados
 # buscando el usuario en la database.
@@ -37,7 +39,12 @@ def SignInView(request):
         else:
             raise PermissionDenied  
                 
-    
+                
+# ########################################################################################### #
+# ##################################    SIGNUP VIEW     ##################################### #
+# ########################################################################################### #            
+                
+                
 # Esta view maneja '/signup'. Renderea el formulario de registro y valida los datos ingresados y guarda el nuevo
 # usuario en la DB
 def SignUpView(request):
@@ -96,7 +103,14 @@ def SignUpView(request):
             return response
     else:
         raise PermissionDenied
-    
+  
+  
+ 
+# ########################################################################################### #
+# ##################################    LOGOUT VIEW     ##################################### #
+# ########################################################################################### # 
+  
+# Esta view maneja '/logout'. Se encarga de eliminar la cookie de identificacion de usuario.   
 def LogOutView(request):
     if request.method == 'GET':
         response = redirect('/')
@@ -105,12 +119,19 @@ def LogOutView(request):
     else:
         raise PermissionDenied
   
+
+# ########################################################################################### #
+# ###############################    PASSWD RECOVER VIEW     ################################ #
+# ########################################################################################### #
+  
 def sendRecoveryEmail(user):
     url = PASSWORD_RECOVERY_URL + user.username
-    message = 'Estimado usuario: \nSi desea recuperar su clave, por favor' + ' ingresar al siguiente link. \n\n\n  ' + url + '\n\n\n Muchas Gracias. UIN Gaming Team'
+    message = 'Estimado usuario: \nSi desea recuperar su clave, por favor' + ' ingresar al siguiente link.\n\n' + url + '\n\nMuchas Gracias. UIN Gaming Team'
     send_mail('UIN Gaming - Sistema de recuperacion de clave',message,
               TESTING_ADDRESS, [user.email],fail_silently=False);
-  
+
+# Esta view maneja '/passwd-recover'. Se encarga de pedir el usuario a recuperar y configurar la cookie de seguridad.
+# Ademas envia el mail de recuperacion de contrasenia
 def PasswordRecoverView(request):
     if request.method == 'GET':
         # GET METHOD: Aca envio el formulario de recuperacion de contrasenia
@@ -141,14 +162,23 @@ def PasswordRecoverView(request):
         return render_to_response('passwd_recover.html',information,RequestContext(request))
     else:
         raise PermissionDenied
-    
+ 
+
+
+# ########################################################################################### #
+# ############################    PASSWD RECOVER FORM VIEW      ############################# #
+# ########################################################################################### #
+  
+# Esta view maneja '/passwd-recover/<user>'. Se encarga de verificar la existencia de la cookie 'lpwd_ok' y cambiar la password
+# del solicitante.
 def PasswordRecoverFormView(request, username):
-    # GET METHOD: Aca envio el formulario de recuperacion de contrasenia
+    # GET METHOD: Aca envio el formulario para el cambio de la contrasenia
     if request.method == 'GET':
         cookie = request.get_signed_cookie(key='lpwd_ok', default=None)
         if cookie is None:
             return redirect('/')
         else:
+            # Encontro una cookie valida. Renderiza el formulario de cambio de clave
             information = {}
             information['username'] = cookie.split('|')[0]
             information['email'] = cookie.split('|')[1]
@@ -157,6 +187,7 @@ def PasswordRecoverFormView(request, username):
             else:
                 return render_to_response('passwd_recover_form.html',information,RequestContext(request))
     elif request.method == 'POST':
+        # POST METHOD: Realizo la validacion de los datos ingresados y cambio la contrasenia
         cookie = request.get_signed_cookie(key='lpwd_ok', default=None)
         if cookie is None:
             return redirect('/')
